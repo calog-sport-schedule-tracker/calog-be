cdf6415d4267982cae6f5ceb9b47085892093ccd
kihwn <cristine980@konkuk.ac.kr>
2024-11-22 14:03:07 +0900

    :pencil2: Fix:Event logic
    full calendar 라이브러리에 맞게 event 데이터 처리 방식 수정

--- Calog/src/main/java/com/calog/model/service/EventService.java
+++ Calog/src/main/java/com/calog/model/service/EventService.java
@@ -8,9 +8,9 @@ import java.util.List;
 public interface EventService {
     public List<Event>getAllEvent();
     public List<Event>getEventByMonth(int eventYear, int eventMonth);
-    public List<Event>getEventBySport(int eventYear, int eventMonth, String sport);
-    public List<Event>getEventByCity(int eventYear, int eventMonth, String city);
-    public List<Event>getEventBySportAndCity(int eventYear, int eventMonth, String sport, String city);
+    public List<Event>getEventBySport(String sport);
+    public List<Event>getEventByCity( String city);
+    public List<Event>getEventBySportAndCity( String sport, String city);
     public List<Event>getEventByDate(int eventYear, int eventMonth, int eventDay);
     public Event getOneEvent(int id);
 
