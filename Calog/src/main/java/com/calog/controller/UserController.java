0826dd13fb4574cbdf8ab139d9b262d7cfb2fab7
kihwn <cristine980@konkuk.ac.kr>
2024-11-25 16:49:23 +0900

    :pencil2: Fix: Add Participation Filter

--- Calog/src/main/java/com/calog/controller/UserController.java
+++ Calog/src/main/java/com/calog/controller/UserController.java
@@ -43,22 +43,38 @@ public class UserController {
         if (user != null) {
 
 //
-            //나중에 jwt로 바꾸겠습니다.
-//            Cookie cookie = new Cookie("LogIn", user.getUserId()); //브라우저 세션
-//            //HTTP 전송만 허용하여 JavaScript 같은 클라이언트 스크립트에서 쿠키에 접근하지 못하게 설정 (보안 강화)
-//            cookie.setHttpOnly(true);
-//            // 쿠키의 유효 경로를 웹 애플리케이션 전체("/")로 설정하여 모든 요청 경로에서 사용 가능하도록 함
-//            cookie.setPath("/");
-//            cookie.setMaxAge(24 * 60 * 60);
-//
-////            //설정한 쿠키를 HTTP 응답에 추가하여 클라이언트에게 전송
-//            response.addCookie(cookie);
+
+            Cookie cookie = new Cookie("LogIn", user.getUserId()); //브라우저 세션
+            //HTTP 전송만 허용하여 JavaScript 같은 클라이언트 스크립트에서 쿠키에 접근하지 못하게 설정 (보안 강화)
+            cookie.setHttpOnly(true);
+            // 쿠키의 유효 경로를 웹 애플리케이션 전체("/")로 설정하여 모든 요청 경로에서 사용 가능하도록 함
+            cookie.setPath("/");
+            cookie.setMaxAge(24 * 60 * 60);
+
+            //설정한 쿠키를 HTTP 응답에 추가하여 클라이언트에게 전송
+            response.addCookie(cookie);
            return ResponseEntity.status(HttpStatus.OK).body(user);
        } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Failed: 다시 로그인하세요");
         }
 //
     }
+
+    @GetMapping("/check-login")
+    public ResponseEntity<?> checkLogin(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("LogIn".equals(cookie.getName())) {
+                    return ResponseEntity.ok(cookie.getValue());
+                }
+            }
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인 필요");
+    }
+
+
+
     @GetMapping("/getAll")
     public ResponseEntity<?>getAllUser(){
         List<User> result = userService.getAllUser();
--- Calog/src/main/java/com/calog/model/service/ParticipationServiceImpl.java
+++ Calog/src/main/java/com/calog/model/service/ParticipationServiceImpl.java
@@ -22,12 +22,23 @@ public class ParticipationServiceImpl implements ParticipationService {
         System.out.println("유저의 모든 참여 기록 조회");
         return pd.selectAllByUserId(userId);
     }
+    @Override
+    public List<Participation> getAllParticipationByUserId2(int userId) {
+        System.out.println("유저의 모든 참여 기록 조회");
+        return pd.selectAllByUserId2(userId);
+    }
 
     @Override
     public List<Participation> getParticipationBySport(int userId, String sport) {
         System.out.println("대회 종목 별 참여 기록 조회");
 
         return pd.selectByUserIdFiltSport(userId, sport);
+    }
+    @Override
+    public List<Participation> getParticipationBySport2(int userId, String sport) {
+        System.out.println("대회 종목 별 참여 기록 조회");
+
+        return pd.selectByUserIdFiltSport2(userId, sport);
     }
 
     @Override
@@ -36,11 +47,22 @@ public class ParticipationServiceImpl implements ParticipationService {
         return pd.selectByUserIdFiltMonth(userId, eventYear, eventMonth);
 
     }
+    @Override
+    public List<Participation> getParticipationByMonth2(int userId, int eventYear, int eventMonth) {
+        System.out.println("월 별 대회 참여 기록 조회");
+        return pd.selectByUserIdFiltMonth2(userId, eventYear, eventMonth);
+
+    }
 
     @Override
     public List<Participation> getParticipationByCity(int userId, String city) {
         System.out.println("대회 참여 장소 별 기록 조회");
         return pd.selectByUserIdFiltCity(userId,  city);
+    }
+    @Override
+    public List<Participation> getParticipationByCity2(int userId, String city) {
+        System.out.println("대회 참여 장소 별 기록 조회");
+        return pd.selectByUserIdFiltCity2(userId,  city);
     }
 
     @Override
@@ -48,15 +70,30 @@ public class ParticipationServiceImpl implements ParticipationService {
         System.out.println("대회 참여 종목&날짜 별 기록 조회");
         return pd.selectByUserIdFiltSportAndMonth(userId,  sport, eventYear, eventMonth);
     }
+    @Override
+    public List<Participation> getParticipationBySportAndMonth2(int userId, String sport, int eventYear, int eventMonth) {
+        System.out.println("대회 참여 종목&날짜 별 기록 조회");
+        return pd.selectByUserIdFiltSportAndMonth2(userId,  sport, eventYear, eventMonth);
+    }
 
     @Override
     public List<Participation> getParticipationBySportAndCity(int userId, String sport, String city) {
         System.out.println("대회 참여 종목&도시 별 기록 조회");
         return pd.selectByUserIdFiltSportAndCity(userId,sport,city);
     }
+    @Override
+    public List<Participation> getParticipationBySportAndCity2(int userId, String sport, String city) {
+        System.out.println("대회 참여 종목&도시 별 기록 조회");
+        return pd.selectByUserIdFiltSportAndCity2(userId,sport,city);
+    }
 
     @Override
     public List<Participation> getParticipationByMonthAndCity(int userId, int eventYear, int eventMonth, String city) {
+        System.out.println("대회 참여 날짜&도시 별 기록 조회");
+        return pd.selectByUserIdFiltMonthAndCity(userId,eventYear,eventMonth,city);
+    }
+    @Override
+    public List<Participation> getParticipationByMonthAndCity2(int userId, int eventYear, int eventMonth, String city) {
         System.out.println("대회 참여 날짜&도시 별 기록 조회");
         return pd.selectByUserIdFiltMonthAndCity(userId,eventYear,eventMonth,city);
     }
@@ -65,6 +102,11 @@ public class ParticipationServiceImpl implements ParticipationService {
     public List<Participation> getParticipationBySportAndMonthAndCity(int userId, String sport, int eventYear, int eventMonth, String city ) {
         System.out.println("대회 참여 종목&날짜&도시 별 기록 조회");
         return pd.selectByUserIdFiltSportAndMonthAndCity(userId,sport,eventYear,eventMonth,city);
+    }
+    @Override
+    public List<Participation> getParticipationBySportAndMonthAndCity2(int userId, String sport, int eventYear, int eventMonth, String city ) {
+        System.out.println("대회 참여 종목&날짜&도시 별 기록 조회");
+        return pd.selectByUserIdFiltSportAndMonthAndCity2(userId,sport,eventYear,eventMonth,city);
     }
 
     @Override
