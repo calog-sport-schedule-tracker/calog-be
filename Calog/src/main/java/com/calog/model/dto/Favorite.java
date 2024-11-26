eb1aeab7bd772126a7b37d503d9f4a52d11c2f6b
kihwn <cristine980@konkuk.ac.kr>
2024-11-26 14:25:17 +0900

    :sparkles: Feat: Make Favorite
    Add Favorite mapper,dao,dto,controller

---
+++ Calog/src/main/java/com/calog/model/dto/Favorite.java
@@ -0,0 +1,42 @@
+package com.calog.model.dto;
+
+import java.time.LocalDateTime;
+
+public class Favorite {
+    private int id;
+    private int userId;
+    private int eventId;
+    private LocalDateTime updatedAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
